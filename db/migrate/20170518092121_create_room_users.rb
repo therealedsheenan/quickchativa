b7d69d724d055f58182ffe577430e69ff677e0a9
therealedsheenan <private.sheenantenepre@gmail.com>
2017-05-18 18:23:26 +0900

    update database schema

---
+++ db/migrate/20170518092121_create_room_users.rb
@@ -0,0 +1,10 @@
+class CreateRoomUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :room_users do |t|
+      t.integer :room_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517090523) do
+ActiveRecord::Schema.define(version: 20170518092121) do
 
   create_table "chats", force: :cascade do |t|
     t.integer "user_id"
     t.text "message"
     t.integer "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "room_users", force: :cascade do |t|
+    t.integer "room_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
